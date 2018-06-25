07a3d596d1f2c63c87a031bb7c7cce6670131ced
Jonas Kuebler <jonikuebler@gmail.com>
2018-06-25 12:40:25 +0200

    Completely restructured Main Predictor Class

--- src/Network/PredictorMain.py
+++ src/Network/PredictorMain.py
@@ -1,122 +1,209 @@
 import time
 
 import numpy as np
+from Bio import SeqIO
 
+from src.Helpers.Result_Evaluator import Result_Evaluator
 from src.Network.NeuralNetwork import NeuralNetwork
 from src.Preprocessing.Preprocessor import Preprocessor
 
-start_time = time.time()
-# TRAINING
-# training Set
-training_set_file = '/ebio/abt1_share/update_tprpred/data/Training_Data/positive_set.txt'
-training_negative_set_file = '/ebio/abt1_share/update_tprpred/Training_Data/negative_set.txt'
-test_set_file = '/ebio/abt1_share/update_tprpred/data/Training_Data/test_set_pos.txt'
-test_negative_set_file = '/ebio/abt1_share/update_tprpred/data/Training_Data/test_set_neg.txt'
 
-multiple_predictions = '/ebio/abt1_share/update_tprpred/data/Test_Proteins/vikram_more.fa'
+def init_preprocessor(input):
 
-#scope_predictions = '/ebio/abt1_share/toolkit_sync/databases/hh-suite/scope70/scope70.fas'
+    # Preprocessing Protein to predict
+    preprocessor_object = Preprocessor(input)
 
-human_proteins = '/ebio/abt1_share/update_tprpred/data/Proteomes/25.H_sapiens.fasta'
-
-multiple_proteins = '/ebio/abt1_share/update_tprpred/data/old_maybe_gold_later/testRandom.txt'
-
-# Test Protein for Prediction
-inputProtein = '/ebio/abt1_share/update_tprpred/data/Test_Proteins/1W3B.fa'
-
-# Preprocessing Protein to predict
-PreprocessorObject = Preprocessor(inputProtein)
-#
-# pos_set = PreprocessorObject.read_line_files(training_set_file)
-# neg_set = PreprocessorObject.read_line_files(training_negative_set_file)
-# pos_test_set = PreprocessorObject.read_line_files(test_set_file)
-# neg_test_set = PreprocessorObject.read_line_files(test_negative_set_file)
-#
-# # Multidimensional Approach encodes Data as (x, 34, 20) Array
-# encoded_pos_set = PreprocessorObject.encoding_training_helper(pos_set)
-# encoded_neg_set = PreprocessorObject.encoding_training_helper(neg_set)
-# encoded_pos_test_set = PreprocessorObject.encoding_training_helper(pos_test_set)
-# encoded_neg_test_set = PreprocessorObject.encoding_training_helper(neg_test_set)
-#
-# # Add Pos and Neg Training set to one Array
-# encoded_total_set = encoded_pos_set + encoded_neg_set
-# encoded_test_total_set = encoded_pos_test_set + encoded_neg_test_set
-#
-# # Convert to np Array
-# encoded_array = np.asarray(encoded_total_set)
-# encoded_array_test = np.asarray(encoded_test_total_set)
-#
-# # Create Labels for Training
-# labels = PreprocessorObject.create_labels(len(encoded_pos_set), len(encoded_neg_set))
-#
-# # Create Labels for Test Set
-# labels_test = PreprocessorObject.create_labels(len(encoded_pos_test_set), len(encoded_neg_test_set))
-
-# Train network with Training and Test Set
-NeuralNetworkObject = NeuralNetwork()
-#
-NeuralNetworkObject.load_model('/ebio/abt1_share/update_tprpred/code/PycharmProjects/TrainingData/src/NetworkData/model.json',
-                               '/ebio/abt1_share/update_tprpred/code/PycharmProjects/TrainingData/src/NetworkData/model.h5')
-
-NeuralNetworkObject.compile_network()
-
-# NeuralNetworkObject.train_network(encoded_array, labels, encoded_array_test, labels_test)
-
-# NeuralNetworkObject.cross_validate(encoded_array, labels, encoded_array_test, labels_test)
-
-# Save and load model to/from json
-# NeuralNetworkObject.save_model()
+    return preprocessor_object
 
 
-# Make prediction
-# Cut Input Protein into windows
-fragments_per_protein = PreprocessorObject.cutInWindows(34)
+def init_network():
 
-print('Cutted in fragments in: ' + "%s seconds" % (str(time.time() - start_time)))
-start_time = time.time()
+    # Train network with Training and Test Set
+    neural_network_object = NeuralNetwork()
 
-prediction_set = PreprocessorObject.encoding_helper(fragments_per_protein)
-
-print('Encoded in : ' + "%s seconds" % (str(time.time() - start_time)))
-start_time = time.time()
-
-new_file = open('/ebio/abt1_share/update_tprpred/data/network_predictions' + '.txt', 'w')
-counter = 0
-foundTPR = False
-
-for protein in prediction_set:
-
-    prediction_set_array = np.asarray(protein)
-    results = NeuralNetworkObject.predict(prediction_set_array)
-
-    initial_findings_index = PreprocessorObject.filter_print_results(results, 0.7)
-
-    for elem in initial_findings_index:
-        foundTPR = True
-        new_file.write('Protein: ' + str(PreprocessorObject.ids[counter]) + '\n')
-        new_file.write("Probability of being a TPR starting at position " + str(elem) + ' '
-                       ':' + str(PreprocessorObject.probabilites[elem]) + '\n')
-
-        new_file.write('Sequence: ' + str(fragments_per_protein[counter][elem - 1].upper()) + '\n')
-
-    counter += 1
-
-if not foundTPR:
-    new_file.write('No TPR repeats found')
-
-print('Results written in : ' + "%s seconds" % (str(time.time() - start_time)))
-
-new_file.close()
+    return neural_network_object
 
 
+def init_training_data(preprocessor_object):
 
-# Run Time improvements
-# for record in SeqIO.parse(PreprocessorObject.inputFile, 'fasta'):
-#
-#     fragments = PreprocessorObject.cutInWindows_single(record, 34)
-#
-#     prediction_set = PreprocessorObject.encoding_helper_single(fragments)
-#
-# print('Single cutting and encoding: ' + "%s seconds" % (str(time.time() - start_time)))
-# start_time = time.time()
+    # Read in training Data
+    pos_set = preprocessor_object.read_line_files(training_set_file)
+    neg_set = preprocessor_object.read_line_files(training_negative_set_file)
+    pos_test_set = preprocessor_object.read_line_files(test_set_file)
+    neg_test_set = preprocessor_object.read_line_files(test_negative_set_file)
 
+    # Multidimensional Approach encodes Data as (x, 34, 20) Array
+    encoded_pos_set = preprocessor_object.encoding_training_helper(pos_set)
+    encoded_neg_set = preprocessor_object.encoding_training_helper(neg_set)
+    encoded_pos_test_set = preprocessor_object.encoding_training_helper(pos_test_set)
+    encoded_neg_test_set = preprocessor_object.encoding_training_helper(neg_test_set)
+
+    # Add Pos and Neg Training set to one Array
+    encoded_total_set = encoded_pos_set + encoded_neg_set
+    encoded_test_total_set = encoded_pos_test_set + encoded_neg_test_set
+
+    # Convert to np Array
+    encoded_array = np.asarray(encoded_total_set)
+    encoded_array_test = np.asarray(encoded_test_total_set)
+
+    # Create Labels for Training
+    labels = preprocessor_object.create_labels(len(encoded_pos_set), len(encoded_neg_set))
+
+    # Create Labels for Test Set
+    labels_test = preprocessor_object.create_labels(len(encoded_pos_test_set), len(encoded_neg_test_set))
+
+    return [encoded_array, labels, encoded_array_test, labels_test]
+
+
+def train_network(network_object, training_data):
+
+    network_object.compile_network()
+
+    network_object.train_network(training_data[0], training_data[1], training_data[2], training_data[3])
+
+
+def load_network(network_object):
+
+    network_object.load_model('/ebio/abt1_share/update_tprpred/code/PycharmProjects/TrainingData/src/NetworkData/model.json',
+                              '/ebio/abt1_share/update_tprpred/code/PycharmProjects/TrainingData/src/NetworkData/model.h5')
+
+    network_object.compile_network()
+
+
+def save_network(network_object):
+
+    network_object.save_model()
+
+
+def cross_validate(network_object, training_data):
+
+    network_object.cross_validate(training_data[0], training_data[1])
+
+
+def predict(network_object, preprocessor_object):
+
+    # Make prediction
+    # Cut Input Protein into windows
+    fragments_per_protein = preprocessor_object.cutInWindows(34)
+
+    prediction_set = preprocessor_object.encoding_helper(fragments_per_protein)
+
+    new_file = open('/ebio/abt1_share/update_tprpred/data/network_predictions' + '.txt', 'w')
+    counter = 0
+    foundTPR = False
+
+    for protein in prediction_set:
+
+        prediction_set_array = np.asarray(protein)
+        results = network_object.predict(prediction_set_array)
+
+        initial_findings_index = preprocessor_object.filter_print_results(results, 0.7)
+
+        for elem in initial_findings_index:
+            foundTPR = True
+            new_file.write('Protein: ' + str(preprocessor_object.ids[counter]) + '\n')
+            new_file.write("Probability of being a TPR starting at position " + str(elem) + ' '
+                           ':' + str(preprocessor_object.probabilites[elem]) + '\n')
+
+            new_file.write('Sequence: ' + str(fragments_per_protein[counter][elem - 1].upper()) + '\n')
+
+        counter += 1
+
+    if not foundTPR:
+        new_file.write('No TPR repeats found')
+
+    new_file.close()
+
+
+def single_predict(network_object, preprocessor_object, threshold):
+
+    # Run Time improvements
+    new_file = open('/ebio/abt1_share/update_tprpred/data/network_predictions' + '.txt', 'w')
+    counter = 0
+    found_tpr = True
+    final_props = []
+    Evaluator = Result_Evaluator()
+
+    for record in SeqIO.parse(preprocessor_object.inputFile, 'fasta'):
+
+        if len(record.seq) > 34:
+
+            fragments = preprocessor_object.cutInWindows_single(record, 34)
+
+            prediction_set = preprocessor_object.encoding_helper_single(fragments)
+
+            prediction_set_array = np.asarray(prediction_set)
+
+            results = network_object.predict(prediction_set_array)
+
+            initial_findings_index = preprocessor_object.filter_print_results(results, threshold)
+
+            for elem in initial_findings_index:
+                new_file.write('Protein: ' + str(preprocessor_object.ids[counter]) + '\n')
+                new_file.write("Probability of being a TPR starting at position " + str(elem) + ' '
+                               ':' + str(preprocessor_object.probabilites[elem]) + '\n')
+
+                final_props.append(preprocessor_object.probabilites[elem])
+
+                new_file.write('Sequence: ' + str(fragments[elem - 1].upper()) + '\n')
+
+            counter += 1
+
+            if not found_tpr:
+                new_file.write('No TPR repeats found')
+
+    print("Average probability: " + str(Evaluator.average_probability(final_props)))
+
+    new_file.close()
+
+
+if __name__ == '__main__':
+
+    start_time = time.time()
+
+    # Positive training set
+    training_set_file = '/ebio/abt1_share/update_tprpred/data/Training_Data/positive_set.txt'
+
+    # Negative training set
+    training_negative_set_file = '/ebio/abt1_share/update_tprpred/data/Training_Data/negativ_set.txt'
+
+    # Positive test set
+    test_set_file = '/ebio/abt1_share/update_tprpred/data/Training_Data/test_set_pos.txt'
+
+    # Negative test set
+    test_negative_set_file = '/ebio/abt1_share/update_tprpred/data/Training_Data/test_set_neg.txt'
+
+    # Different Test Files
+    test_one = '/ebio/abt1_share/update_tprpred/data/Test_Proteins/vikram_more.fa'
+    test_two = '/ebio/abt1_share/update_tprpred/data/__old_maybe_gold_later/testRandom.txt'
+    test_three = '/ebio/abt1_share/update_tprpred/data/Test_Proteins/Vikram_Uniprot.fa'
+
+    # Scope70 Fasta
+    # scope_predictions = '/ebio/abt1_share/toolkit_sync/databases/hh-suite/scope70/scope70.fas'
+
+    # EColi proteom
+    e_coli = '/ebio/abt1_share/update_tprpred/data/Proteomes/Escherichia_coli_K12.fas'
+
+    # Initialize necessary objects
+    Evaluator = Result_Evaluator()
+    Preprocessor = init_preprocessor(e_coli)
+    Network = init_network()
+    Training_data = init_training_data(Preprocessor)
+    #
+    train_network(Network, Training_data)
+
+    # save_network(Network)
+
+    # load_network(Network)
+
+    single_predict(Network, Preprocessor, 0.75)
+
+
+    # print('Cutted in fragments in: ' + "%s seconds" % (str(time.time() - start_time)))
+    # start_time = time.time()
+    #
+    # print('Encoded in : ' + "%s seconds" % (str(time.time() - start_time)))
+    # start_time = time.time()
+    #
+    #
+    # print('Results written in : ' + "%s seconds" % (str(time.time() - start_time)))
