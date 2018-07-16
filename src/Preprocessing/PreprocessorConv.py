726dff4dcbfbfc4ede8798d2fe7190987ed8db62
Jonas Kuebler <jonikuebler@gmail.com>
2018-07-16 18:34:56 +0200

    Added PreprocessorConv class file

---
+++ src/Preprocessing/PreprocessorConv.py
@@ -0,0 +1,11 @@
+# Created by Jonas Kuebler on 05/15/2018
+
+
+class PreprocessorConv:
+
+    def __init__(self, query_match_file):
+
+        # .match output file of master
+        self.input_file = query_match_file
+
+
